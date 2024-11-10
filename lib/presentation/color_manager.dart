40a9c9c2a4af1358c5b17980ae602fce8a1d96ed
yehiaelkenany-dev <elkenanyyehia@gmail.com>
2024-11-10 20:31:36 +0200

    Creating Color Manager

---
+++ lib/presentation/color_manager.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static Color primary = const Color(0xFFED9728);
+  static Color darkGrey = const Color(0xFF525252);
+  static Color grey = const Color(0xFF737477);
+  static Color lightGrey = const Color(0xFF9E9E9E);
+}
