f07408c05e20abff3b82e56ef6bbf4cfcbd37973
yehiaelkenany-dev <elkenanyyehia@gmail.com>
2024-11-10 21:13:46 +0200

    Creating text style manager

---
+++ lib/presentation/styles_manager.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:weather_now/presentation/font_manager.dart';
+
+TextStyle _getTextStyle(double fontSize, FontWeight fontWeight, Color color) {
+  return TextStyle(
+      fontSize: fontSize,
+      fontFamily: FontConstants.fontFamily,
+      color: color,
+      fontWeight: fontWeight);
+}
+
+TextStyle getRegularTextStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(fontSize, FontWeightManager.regular, color);
+}
+
+TextStyle getMediumTextStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(fontSize, FontWeightManager.medium, color);
+}
+
+TextStyle getLightTextStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(fontSize, FontWeightManager.light, color);
+}
+
+TextStyle getBoldTextStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(fontSize, FontWeightManager.bold, color);
+}
+
+TextStyle getSemiBoldTextStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(fontSize, FontWeightManager.semiBold, color);
+}
